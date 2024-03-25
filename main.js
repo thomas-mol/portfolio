130f54854f2737ba5b417027494d53dcedf27514
Thomas Mol <s130286@ap.be>
2024-03-25 11:46:30 +0100

    Add scroll fade in animation

--- main.js
+++ main.js
@@ -36,4 +36,17 @@ document.addEventListener("DOMContentLoaded", () => {
       document.querySelector("#scroll-top").classList.remove("active");
     }
   });
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      } else {
+        entry.target.classList.remove("show");
+      }
+    });
+  });
+
+  const hiddenElements = document.querySelectorAll(".hidden");
+  hiddenElements.forEach((el) => observer.observe(el));
 });
